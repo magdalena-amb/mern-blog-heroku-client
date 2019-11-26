f2c845d0bf875b7d81b103ff4c352d17044ee533
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-11-26 09:18:14 +0100

    Modify files

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
-export const API_URL = 'http://localhost:8000/api';
+export const API_URL = "https://blog-app-mern-m-server.herokuapp.com/api";
 
 export const BASE_URL = 'http://localhost:3000/${location.pathname}';
+
+
+//'http://localhost:8000/api';
